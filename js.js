e7ebae754839f9b058f856d6338fff3147e2d50a
Herman0404 <herminator04@gmail.com>
2024-12-07 05:08:59 +0100

    darkmode, answers, score
    Ändrade namn på darkmode funktion.
    Lade till separata funktioner beroende på om man valde sant/falskt.
    answerCheck() kollar om valda svar är korrekt och ger ut poäng beroende på rätt eller fel.
    Lade till Frågnummer över frågan.

--- js.js
+++ js.js
@@ -1,55 +1,67 @@
 let qNum = 0;
 let t = true;
 let f = false;
-let q1 = "10 / 0 = 0";
-let q2 = "17 * 6 = 102";
-let q3 = "64 / 4 = 16";
-let q4 = "17 / 3 = 7";
-let q5 = "93 / 3 = 31";
-let q6 = "14 - 12 + 7 - 3 + 9 = 15";
-let q7 = "0.5^2 = 0.25";
-let q8 = "14 % 9 = 5"
-let q9 = "36 * 27 = 962"
-let q10 = "743 % 21 = 6"
 
 const qaArray = [
-    [q1, f, 0],
-    [q2, t, 0],
-    [q3, t, 0],
-    [q4, f, 0],
-    [q5, t, 0],
-    [q6, t, 0],
-    [q7, t, 0], 
-    [q8, t, 0], 
-    [q9, f, 0], 
-    [q10, f, 0]
+    ["10 / 0 = 0", f, 0],
+    ["17 * 6 = 102", t, 0],
+    ["64 / 4 = 16", t, 0],
+    ["17 / 3 = 7", f, 0],
+    ["93 / 3 = 31", t, 0],
+    ["14 - 12 + 7 - 3 + 9 = 15", t, 0],
+    ["0.5^2 = 0.25", t, 0],
+    ["14 % 9 = 5", t, 0],
+    ["36 * 27 = 962", f, 0],
+    ["743 % 21 = 6", f, 0]
 ];
 
 function startQuiz(){
-    var quiz = document.querySelector("#quiz");
-    var start = document.querySelector("#startButton");
+    const quiz = document.querySelector("#quiz");
+    const start = document.querySelector("#startButton");
+
     quiz.classList.toggle("hidden");
     start.classList.toggle("hidden");
-    document.getElementById("quizQuestion").innerHTML = qaArray[qNum][0];
-    qNum++;
+    questionUpdate();
 }
 
-function questionAnswer(){
-    if (qNum < qaArray.length) {
-        document.getElementById("quizQuestion").innerHTML = qaArray[qNum][0];
-        qNum++;
-    } 
-    
-    else {
+function questionUpdate(){
+    document.getElementById("quizQNum").innerHTML = "Question " + (qNum + 1)    ;
+    document.getElementById("quizQuestion").innerHTML = qaArray[qNum][0];
+}
+
+
+function questionAnswer(ans) {
+    if (qNum >= (qaArray.length-1)) {
+        answerCheck(ans);
         let score = 0;
-        for (i = 0; i < qaArray.length; i++){
+        for (let i = 0; i < qaArray.length; i++) {
             score += qaArray[i][2];
         }
         document.getElementById("quizQuestion").innerHTML = score;
+    } else {
+        answerCheck(ans);
+        qNum++;  
+        questionUpdate();  
     }
 }
 
-function myFunction() {
+
+function qTrue(){
+    questionAnswer(t);
+}
+
+function qFalse(){
+    questionAnswer(f);
+}
+
+function answerCheck(ans){
+    if(ans == qaArray[qNum][1]){
+        qaArray[qNum][2] = 1;
+    }
+}
+
+
+function darkModeFunc() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
