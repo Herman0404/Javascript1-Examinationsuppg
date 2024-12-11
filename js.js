6bc5cde46d3621dc9456d75476c710f7dbfeb95e
Herman0404 <herminator04@gmail.com>
2024-12-11 07:26:24 +0100

    Slutuppdatering (antagligen)
    Lade till rättning för checkbox, ändrade i disableInputs så light/darkmode alltid går att byta mellan. Lagt till kommentarer. Ändrat namn på klasser och variabler. Fixat utskrift av resultat. Gjort återkommande kod till funktioner. Ändrade strukturen på checkboxfrågor inom HTML.

--- js.js
+++ js.js
@@ -1,48 +1,51 @@
-const cBoxAns = [
-    ["1","-1"],
-    ["0.5", "-2"],
-    ["2","-3"],
-    ["2","-1"]
-]
-
-let points = 0;
-
+// Funktionen för inlämning
 function submit(){
+    let score = 0;
+    let maxScore = 16;
     disableInputs();
-    ToFCheck();
-    multiOptsCheck();
-    console.log(points);
+    score = ToFCheck(score);
+    score = multiOptsCheck(score);
+    score = cBoxCheck(score);
+    showScore(score, maxScore);
 }
 
-function ToFCheck() {
+// RADIOBUTTONS
+
+// True or False rättning
+function ToFCheck(score) {
     const ToFanswers = []; // Användarens svar
     const ToFAns = ["False", "True", "True", "False"]; // Facit 
 
-    const questions = document.querySelectorAll(".ToF"); // Query för alla frågor
+    const questions = document.querySelectorAll(".ToF"); // Query för allt inom klassen .ToF
 
-    radioButtonCheck(ToFanswers, ToFAns, questions);
+    score = radioButtonCheck(ToFanswers, ToFAns, questions, score);
+
+    return score;
 }
 
-function multiOptsCheck() {
+// multiOptions rättning
+function multiOptsCheck(score) {
     const optAnswers = []; // Användarens svar
-    const optAns = ["2i", "9/4", "2", "3"];
+    const optAns = ["2i", "9/4", "2", "3"]; // Facit
     
 
-    const questions = document.querySelectorAll(".multiOpt"); // Query för alla frågor
+    const questions = document.querySelectorAll(".multiOpt"); // Query för allt inom klassen multiOpt
 
-    radioButtonCheck(optAnswers, optAns, questions);
+    score = radioButtonCheck(optAnswers, optAns, questions, score);
+
+    return score;
 }
 
 
 
-function radioButtonCheck(userAns, Facit, questions){
+function radioButtonCheck(userAns, Facit, questions, score){
     questions.forEach((question, id) => {
-        const checked = question.querySelector("input:checked"); // Ta valda radioknappen
+        const checked = question.querySelector("input:checked"); // Ta "checked" radioknappar
         if (checked) {
             userAns.push(checked.value); // Spara valda värdet
         } else {
             userAns.push("Null"); // Vid inget svar, ge null
-            question.classList.add("unanswered"); // Markera frågan som obesvarad med klassen "unanswered"
+            question.classList.add("empty"); // Markera frågan som obesvarad med klassen "empty"
         }
 
         const labels = question.querySelectorAll("label"); // Spara alla labels inom div som söks
@@ -51,38 +54,119 @@ function radioButtonCheck(userAns, Facit, questions){
         if (userAns[id] === Facit[id]) { // Kolla om svar matchar facit
 
             // Vid rätt, ge label som tillhör valda svaret klassen "correct"
-            labels.forEach(label => {
-                if (label.previousElementSibling.value === userAns[id]) {
-                    label.classList.add("correct");
-                }
-            });
+            LabelAssign(labels, userAns[id], "correct");
+            score++; // Öka score
 
-            points++; // Lägg även till poäng till summan
-
-        } else if (userAns[id] !== "Null") { // Koll om svar inte är "Null" (i andra ord ifall frågan besvarats men ändå blev fel)
+        } else if (userAns[id] !== "Null") { // Koll om svar inte är "Null" (ifall frågan besvarats men ändå blev fel)
 
             // Vid fel (inte tomt), ge label som tillhör valda svaret klassen "incorrect"
-            labels.forEach(label => {
-                if (label.previousElementSibling.value === userAns[id]) {
-                    label.classList.add("incorrect");
+            LabelAssign(labels, userAns[id], "incorrect");
+        }
+    });
+
+    return score;
+}
+
+//Checkbox rättning
+
+function cBoxCheck(score) {
+    const cBoxAns = [ // facit
+        ["1", "-1"],
+        ["0.5", "-2"],
+        ["2", "-3"],
+        ["2", "-1"]
+    ];
+
+    const questions = document.querySelectorAll(".cBox"); // Query för allt inom klassen cBox
+
+    questions.forEach((question, id) => { // Loop igenom allt i "questions"
+        const checked = question.querySelectorAll("input:checked"); // Spara "checked" inputs
+        const labels = question.querySelectorAll("label"); // Spara labels
+
+        if (checked.length === 0) {
+            question.classList.add("empty");  // Lägg till "empty" om 0 svar gavs på någon fråga
+        }
+
+        checked.forEach(input => {
+            const selectedValue = input.value; // Användarens input (svar)
+
+            let isCorrect = false; // boolean som sparar om svaret givet är rätt eller fel
+
+            cBoxAns[id].forEach(correctVal => {
+                if (selectedValue === correctVal) {
+                    // Ändra isCorrect till true så det inte markeras som fel senare
+                    isCorrect = true;
+                    LabelAssign(labels, correctVal, "correct");
+                    score++;  // Öka score
                 }
             });
+    
+            // Vid fel svar, ge klass "incorrect"
+            if (!isCorrect) {
+                LabelAssign(labels, selectedValue, "incorrect");
+                score--; // Sänk score (undviker att checka alla checkbox för perfekt score)
+            }
+        });
+    });
+
+    return score;
+
+}
+
+
+//Other
+
+// Utskrift för poäng
+function showScore(score, maxScore) {
+
+    let scoreId = document.getElementById("score"); // förkorttande variabel
+    let scoreInfoId = document.getElementById("scoreInfo"); // förkortande variabel
+
+    scoreId.innerHTML = score + "p/" + maxScore + "p" // utskrift av poäng
+
+    if(score > maxScore*0.75){ // >75% imponerad meddelande 
+        scoreId.style.color = "green";
+        scoreInfoId.innerHTML = "Above 75%. Great job, your hard work shows!";
+        scoreInfoId.style.color = "green";
+
+    } else if (score >= maxScore*0.5){ // 50%-75% passing meddalande
+        scoreId.style.color = "orange";
+        scoreInfoId.innerHTML = "Between 50% and 75% good job.";
+        scoreInfoId.style.color = "orange";
+
+    } else { // <50% fail meddelande
+        scoreId.style.color = "red";
+        scoreInfoId.innerHTML = "Less than 50%. Please study and try again later.";
+        scoreInfoId.style.color = "red";
+
+    }
+}
+
+// Färgandet av backgrund vid label för att visa rätt/fel
+function LabelAssign(labels, value, ans){
+    labels.forEach(label => {
+        const inputElement = label.previousElementSibling;  // Hitta label som passar knapp/checkbox
+        if (inputElement && inputElement.value === value) { 
+            label.classList.add(ans);  // Lägg till "correct" eller "incorrect" klass 
         }
     });
 }
 
 
+// Input avstängning
 function disableInputs() {
-
-    const inputs = document.querySelectorAll("input[type='radio'], input[type='checkbox']"); // Spara typer av inputs
+    const inputs = document.querySelectorAll("input[type='radio'], input[type='checkbox']"); // Samlar ihop alla inputs
     
-    // Stäng av inputs med en loop
-    inputs.forEach(input => { 
-        input.disabled = true;
+    // Stänger av alla inputs förutom darkMode
+    inputs.forEach(input => {
+        if (input.id !== "dMode") {
+            input.disabled = true;
+        }
     });
 }
 
+// Darkmode funktionen
 function darkModeFunc() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+    var element = document.body; // Förkortande variabel
+    element.classList.toggle("dark-mode"); // Lägger darkmode på body
 }
